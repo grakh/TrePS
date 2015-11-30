6c801a3827ef308902c14cb4e4f748bda3654ac0
Serj Klimov <don@list.ru>
2015-11-30 16:09:37 +0300

    v 3.0b

--- src/ru/list/don/GUIMain.java
+++ src/ru/list/don/GUIMain.java
@@ -30,7 +30,7 @@ public class GUIMain {
 	String[] infoLine, listik, treangle;
     JProgressBar progressBar;
     JScrollPane scroll;
-    String path, pathOpen, version = "Version 2.3b";
+    String path, pathOpen, version = "Version 3.0b";
 	int count = 0;
 	OpenPath oPath = new OpenPath();
     
